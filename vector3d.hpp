f2c052ecc07a18aef4a1db59a30c60cd8874b2fb
Mikhail Gorbun <piram13@gmail.com>
2016-10-20 16:21:59 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -4,7 +4,22 @@
 #include <ostream>
 
 class vector3d {
-public:
+    private: int x=0; int y=0; int z=0;
+    public:
+    vector3d( int x1, int y1, int z1) : x(x1), y(y1), z(z1) {};
+    vector3d( int x1, int y1) : x(x1), y(y1) {};
+    vector3d( int x1) : x(x1) {};
+    vector3d() : {};
+    int getx() const { return x; }
+    int gety() const { return y; }
+    int getz() const { return z; }
+    std::ostream &operator<<(std::ostream &stream, const vector3d& a) { return stream << "(" << a.getx() << "," << a.gety() << "," << a.getz() << ")";}
+    vector3d operator+(const vector3d &a) {
+        int commonx = a.getx() + getx();
+        int commony = a.gety() + gety();
+        int commonz = a.getz() + getz();
+        return vector3d(commonx, commony, commonz);
+    }
     const static vector3d X;
     const static vector3d Y;
     const static vector3d Z;
