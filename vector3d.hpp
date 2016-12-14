81b06b73289f539196e87fe76a7a4d3bbd943770
Mikhail Gorbun <piram13@gmail.com>
2016-12-14 19:54:53 +0300

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -4,59 +4,36 @@
 #include <ostream>
 
 class vector3d {
-    public:
-    int x=0; int y=0; int z=0;
-    vector3d( int x1, int y1, int z1) : x(x1), y(y1), z(z1) {};
-    vector3d( int x1, int y1) : x(x1), y(y1) {};
-    vector3d( int x1) : x(x1) {};
-    vector3d() : {};
-    int getx() const { return x; }
-    int gety() const { return y; }
-    int getz() const { return z; }
-    vector3d operator+(const vector3d &a) {
-        int commonx = a.getx() + getx();
-        int commony = a.gety() + gety();
-        int commonz = a.getz() + getz();
-        return vector3d(commonx, commony, commonz);
-    }
-    vector3d operator-(const vector3d &a) {
-        int commonx = a.getx() - getx();
-        int commony = a.gety() - gety();
-        int commonz = a.getz() - getz();
-        return vector3d(commonx, commony, commonz);
-    }
-    vector3d operator^(const vector3d &a) {
-        int commonx = a.gety() * getz() - a.getz() * gety();
-        int commony = a.getz() * getx() - a.getx() * getz();
-        int commonz = a.getx() * gety() - a.gety() * getx();
-        return vector3d(commonx, commony, commonz);
-    }
-    int operator*(const vector3d &a) {
-        int commonx = a.getx() * getx();
-        int commony = a.gety() * gety();
-        int commonz = a.getz() * getz();
-        return commonx + commony + commonz;
-    }
-    vector3d operator*(const int a) {
-        int commonx = a * getx();
-        int commony = a * gety();
-        int commonz = a * getz();
-        return commonx + commony + commonz;
-    }
+    friend vector3d operator*(const vector3d &b, const int a);
+    friend vector3d operator*(const int a, const vector3d &b);
+    friend bool operator==(const vector3d &a, const vector3d &b);
+    friend bool operator!=(const vector3d &a, const vector3d &b);
+    friend std::ostream &operator<<(std::ostream &stream, const vector3d &a);
 
-
-
-
-
+public:
     const static vector3d X;
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
 
-    // TODO: дописать
+    double x;
+    double y;
+    double z;
+    vector3d();
+    vector3d(int x, int y, int z);
+    vector3d(double x, double y, double z);
+
+    double length();
+    void normalize();
+
+    vector3d operator+(const vector3d &a);
+    vector3d operator+(const vector3d &a) const;
+    vector3d operator-(const vector3d &a);
+    vector3d operator-();
+    vector3d operator-() const;
+    vector3d operator^(const vector3d &a);
+    vector3d operator^(const vector3d &a) const;
+    double operator*(const vector3d &a);
 
 };
-  std::ostream &operator<<(std::ostream &stream, const vector3d& a) { return stream << "{" << a.getx() << "," << a.gety() << "," << a.getz() << "}";}
-// TODO: дописать
-
 #endif
